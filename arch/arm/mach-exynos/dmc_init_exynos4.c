8f28bf5657c1fb50017970a525bdd490f7b65224
donglinx <donglinx.peng@intel.com>
2015-12-24 19:37:20 +0800

    Add memory controler init code.
    This is the early version, memory may not work.

--- arch/arm/mach-exynos/dmc_init_exynos4.c
+++ arch/arm/mach-exynos/dmc_init_exynos4.c
@@ -28,6 +28,28 @@
 #include "common_setup.h"
 #include "exynos4_setup.h"
 
+#ifdef CONFIG_TINY4412
+struct mem_timings mem = {
+	.direct_cmd_msr = {
+		DIRECT_CMD1, DIRECT_CMD2, DIRECT_CMD3, DIRECT_CMD4
+	},
+	.timingref = 0x000000BB,
+	.timingrow = 0x4046654f,
+	.timingdata = 0x46400506,
+	.timingpower = 0x52000A3C,
+	.zqcontrol = 0xE3854C03,
+	.control0 = 0x71101008,
+	.control1 = 0xe0000086,
+	.control2 = 0x00000000,
+	.concontrol = 0x0FFF301A,
+	.prechconfig = 0xff000000,
+	.memcontrol = 0x00312640,
+	.memconfig0 = 0x40e01323,
+	.memconfig1 = 0x60e01323,
+	.dll_resync = FORCE_DLL_RESYNC,
+	.dll_on = DLL_CONTROL_ON,
+};
+#else
 struct mem_timings mem = {
 	.direct_cmd_msr = {
 		DIRECT_CMD1, DIRECT_CMD2, DIRECT_CMD3, DIRECT_CMD4
@@ -48,6 +70,8 @@ struct mem_timings mem = {
 	.dll_resync = FORCE_DLL_RESYNC,
 	.dll_on = DLL_CONTROL_ON,
 };
+#endif
+
 static void phy_control_reset(int ctrl_no, struct exynos4_dmc *dmc)
 {
 	if (ctrl_no) {
@@ -124,6 +148,10 @@ static void dmc_init(struct exynos4_dmc *dmc)
 	writel(mem.memconfig0, &dmc->memconfig0);
 	writel(mem.memconfig1, &dmc->memconfig1);
 
+#ifdef CONFIG_TINY4412
+	writel(0x8000001F, &dmc->ivcontrol);
+#endif
+
 	/* Config Precharge Policy */
 	writel(mem.prechconfig, &dmc->prechconfig);
 	/*
@@ -175,6 +203,7 @@ void mem_ctrl_init(int reset)
 	 * 0: full_sync
 	 */
 	writel(1, ASYNC_CONFIG);
+#ifndef CONFIG_TINY4412
 #ifdef CONFIG_ORIGEN
 	/* Interleave: 2Bit, Interleave_bit1: 0x15, Interleave_bit0: 0x7 */
 	writel(APB_SFR_INTERLEAVE_CONF_VAL, EXYNOS4_MIU_BASE +
@@ -183,10 +212,8 @@ void mem_ctrl_init(int reset)
 	writel(APB_SFR_ARBRITATION_CONF_VAL, EXYNOS4_MIU_BASE +
 		APB_SFR_ARBRITATION_CONF_OFFSET);
 #else
-#if 0
 	writel(APB_SFR_INTERLEAVE_CONF_VAL, EXYNOS4_MIU_BASE +
 		APB_SFR_INTERLEAVE_CONF_OFFSET);
-#endif
 	writel(INTERLEAVE_ADDR_MAP_START_ADDR, EXYNOS4_MIU_BASE +
 		ABP_SFR_INTERLEAVE_ADDRMAP_START_OFFSET);
 	writel(INTERLEAVE_ADDR_MAP_END_ADDR, EXYNOS4_MIU_BASE +
@@ -204,6 +231,7 @@ void mem_ctrl_init(int reset)
 		ABP_SFR_SLV1_SINGLE_ADDRMAP_END_OFFSET);
 	writel(APB_SFR_SLV_ADDR_MAP_CONF_VAL, EXYNOS4_MIU_BASE +
 		ABP_SFR_SLV_ADDRMAP_CONF_OFFSET);
+#endif
 #endif
 #endif
 	/* DREX0 */
